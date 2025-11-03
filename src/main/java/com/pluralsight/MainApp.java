e63772aa5551c446a0ec4e57856fa10bf5b36e03
Mateo Morales <Moralmat76@gmail.com>
2025-11-03 17:31:32 -0600

    initial commit - case 1 done

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -3,36 +3,73 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainApp {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("""
+        ArrayList<Product> inventory = new ArrayList<>();
+        ArrayList<Product> cart = new ArrayList<>();
+
+        boolean programRunning = true;
+        while (programRunning) {
+            System.out.println("""
                 Welcome to some Store
                 select an option:
                 1.) Display Products
                 2.) Display Cart
                 3.) Exit """);
 
-        int userChoice = scanner.nextInt();
-        scanner.nextLine();
+            int userChoice = scanner.nextInt();
+            scanner.nextLine();
 
-        boolean mainRunning = true;
-        while (mainRunning) {
             switch (userChoice) {
                 case 1 -> {
+                    Boolean AjithsStatement = true;
 //                    displayAllItems();
-                    BufferedReader bufferedReader = new BufferedReader(new FileReader("product.csv"));
+                    printAllProducts(inventory);
                 }
                 case 2 -> {
 //                    displayCartItems();
                 }
                 case 3 -> {
-                    mainRunning = false;
+                    System.out.println("Bye");
                 }
-                default -> System.out.println("Invalid Answer");
+                default -> System.out.println("Invalid Number");
             }
         }
+    }
+
+    private static void printAllProducts(ArrayList<Product> inventory) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("products.csv"));
+        bufferedReader.readLine();
+        String line;
+        while ((line = bufferedReader.readLine()) !=null) {
+            Product products = parseProduct(line);
+            if (products != null) {
+                inventory.add(products);
+            }
+        }
+        for (Product product : inventory) {
+            System.out.println(product);
+        }
+    }
+
+    private static Product parseProduct(String line) {
+        String[] parts = line.split("\\|");
+        if (parts.length == 4) {
+            String sku = parts[0];
+            String productName = parts[1];
+            double price = Double.parseDouble(parts[2]);
+            String department = parts[3];
+
+            return  new Product(sku, productName, (int) price, department);
+        } else {
+            System.out.println("Invalid");
+        }
+        return null;
     }
 }
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,13 +1,25 @@
 package com.pluralsight;
 
 public class Product {
+    private String sku;
     private String productName;
-    private int price;
+    private double price;
     private String department;
 
-    public Product(String productName, int price, String department) {
+    public Product(String sku, String productName, double price, String department) {
+        this.sku = sku;
         this.productName = productName;
         this.price = price;
         this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Product Name: %s
+                SKU: %s
+                Price: %.2f
+                Department: %s
+                """,productName, sku, price, department);
     }
 }
